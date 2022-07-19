46c0b5267a4c99a17f64a6a8b7270db24a6ecc46
blatantBabbage <ransinghbel@gmail.com>
2022-07-19 16:38:44 +0530

    minor update 2

--- src/com/datastructures/array/interviewquestions/exercises/MaxProductOfTwoInt.java
+++ src/com/datastructures/array/interviewquestions/exercises/MaxProductOfTwoInt.java
@@ -5,19 +5,17 @@ import java.util.Arrays;
 public class MaxProductOfTwoInt {
     public static void maxProduct(int[] intArray) {
         int product = 0;
-        int[] maxProductIndex = new int[2];
+        String pair = "";
         for(int i=0; i<intArray.length; i++) {
             for(int j = i+1; j<intArray.length; j++) {
                 if(intArray[i]*intArray[j] > product) {
                     product = intArray[i]*intArray[j];
-                    maxProductIndex[0] = intArray[i];
-                    maxProductIndex[1] = intArray[j];
+                    pair = Integer.toString(intArray[i]) + "," + Integer.toString(intArray[j]);
                 }
-                continue;
             }
         }
         System.out.println("Max product is : " + product);
-        System.out.println("Max product is found at index : " + Arrays.toString(maxProductIndex));
+        System.out.println("Max product is found at index : " + pair);
     }
 
     public static void main(String[] args) {
