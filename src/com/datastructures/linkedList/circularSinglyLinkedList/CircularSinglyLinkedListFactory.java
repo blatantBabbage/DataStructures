67d6eec310dee34a9d66818527a0b5035d13c059
blatantBabbage <ransinghbel@gmail.com>
2022-10-08 13:55:34 +0530

    Add insertion method for circular singly linked list

--- src/com/datastructures/linkedList/circularSinglyLinkedList/CircularSinglyLinkedListFactory.java
+++ src/com/datastructures/linkedList/circularSinglyLinkedList/CircularSinglyLinkedListFactory.java
@@ -14,4 +14,32 @@ public class CircularSinglyLinkedListFactory {
         tail =node;
         size = 1;
     }
+
+    // Insert node in linked list at a specified location
+    public void insertNode(int valueOfNode, int location) {
+        Node node = new Node();
+        node.nodeValue = valueOfNode;
+        if(head == null) {
+            createNode(valueOfNode);
+            return;
+        } else if (location == 0) {
+            node.nodeReference = head;
+            head = node;
+            tail.nodeReference = head;
+        } else if (location >= size) {
+            node.nodeReference = head;
+            tail.nodeReference = node;
+            tail = node;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index <= location) {
+                tempNode = tempNode.nodeReference;
+                index++;
+            }
+            node.nodeReference = tempNode;
+            tempNode.nodeReference = node;
+        }
+        size++;
+    }
 }
--- src/com/datastructures/linkedList/circularSinglyLinkedList/ExecuteCircularSinglyLinkedList.java
+++ src/com/datastructures/linkedList/circularSinglyLinkedList/ExecuteCircularSinglyLinkedList.java
@@ -5,5 +5,11 @@ public class ExecuteCircularSinglyLinkedList {
         CircularSinglyLinkedListFactory circularSinglyLinkedListFactory = new CircularSinglyLinkedListFactory();
         circularSinglyLinkedListFactory.createNode(7);
         System.out.println(circularSinglyLinkedListFactory.head.nodeValue);
+        circularSinglyLinkedListFactory.insertNode(5,0);
+        System.out.println(circularSinglyLinkedListFactory.head.nodeValue);
+        circularSinglyLinkedListFactory.insertNode(7,7);
+        System.out.println(circularSinglyLinkedListFactory.tail.nodeValue);
+        circularSinglyLinkedListFactory.insertNode(6,1);
+        System.out.println(circularSinglyLinkedListFactory.head.nodeValue);
     }
 }
