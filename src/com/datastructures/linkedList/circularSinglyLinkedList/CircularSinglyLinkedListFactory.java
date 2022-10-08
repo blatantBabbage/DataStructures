2a1bd6d61124889a6a995635a41d938793f80804
blatantBabbage <ransinghbel@gmail.com>
2022-10-08 14:08:24 +0530

    Add comments to insertion method for circular singly linked list

--- src/com/datastructures/linkedList/circularSinglyLinkedList/CircularSinglyLinkedListFactory.java
+++ src/com/datastructures/linkedList/circularSinglyLinkedList/CircularSinglyLinkedListFactory.java
@@ -5,10 +5,11 @@ public class CircularSinglyLinkedListFactory {
     public Node tail;
     public int size;
 
-    // create a node
+    // create a single node
     public void createNode(int valueOfNode) {
         Node node = new Node();
         node.nodeValue = valueOfNode;
+        // nodeReference is referencing itself here as it's a circular singly linked list
         node.nodeReference = node;
         head = node;
         tail =node;
@@ -23,23 +24,34 @@ public class CircularSinglyLinkedListFactory {
             createNode(valueOfNode);
             return;
         } else if (location == 0) {
+            /* to insert a node at the beginning */
+            // newly inserted node points to next node, whose reference was stored in head
             node.nodeReference = head;
+            // head is updated to point at newly inserted node at the beginning
             head = node;
+            // last node is updated to point at newly inserted node at the beginning
             tail.nodeReference = head;
         } else if (location >= size) {
+            // newly inserted node points at the end needs to point to node at beginning whose reference is stored in head
             node.nodeReference = head;
+            // older node at the end needs to point at newly inserted node at the end
             tail.nodeReference = node;
+            // tail should point to new node inserted at the end
             tail = node;
         } else {
+            // creating a temp node and assigning head reference to it, so that we can loop from the beginning
             Node tempNode = head;
             int index = 0;
-            while (index <= location) {
+            while (index <= location-1) {
                 tempNode = tempNode.nodeReference;
                 index++;
             }
+            // pointing the newly inserted node to the next node by taking its reference from current node
             node.nodeReference = tempNode;
+            // updating current node to point to newly inserted node
             tempNode.nodeReference = node;
         }
+        // increment the size of linked list, everytime insertion operation is performed
         size++;
     }
 }
