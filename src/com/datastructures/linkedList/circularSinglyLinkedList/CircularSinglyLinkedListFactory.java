fbd1ccc08961bc1058cb626918b5cd774bf030ec
blatantBabbage <ransinghbel@gmail.com>
2022-10-09 11:09:50 +0530

    Fix bugs in circular singly linked list insertion method

--- src/com/datastructures/linkedList/circularSinglyLinkedList/CircularSinglyLinkedListFactory.java
+++ src/com/datastructures/linkedList/circularSinglyLinkedList/CircularSinglyLinkedListFactory.java
@@ -32,26 +32,42 @@ public class CircularSinglyLinkedListFactory {
             // last node is updated to point at newly inserted node at the beginning
             tail.nodeReference = head;
         } else if (location >= size) {
-            // newly inserted node points at the end needs to point to node at beginning whose reference is stored in head
-            node.nodeReference = head;
             // older node at the end needs to point at newly inserted node at the end
             tail.nodeReference = node;
             // tail should point to new node inserted at the end
             tail = node;
+            // newly inserted node points at the end needs to point to node at beginning whose reference is stored in head
+            node.nodeReference = head;
         } else {
             // creating a temp node and assigning head reference to it, so that we can loop from the beginning
             Node tempNode = head;
             int index = 0;
-            while (index <= location-1) {
+            while (index < location-1) {
                 tempNode = tempNode.nodeReference;
                 index++;
             }
             // pointing the newly inserted node to the next node by taking its reference from current node
-            node.nodeReference = tempNode;
+            node.nodeReference = tempNode.nodeReference;
             // updating current node to point to newly inserted node
             tempNode.nodeReference = node;
         }
         // increment the size of linked list, everytime insertion operation is performed
         size++;
+    }
+
+    /* Circular singly linked list traversal */
+    public void traverseCircularSinglyLinkedList() {
+        Node tempNode = head;
+        if(head != null) {
+            for (int i = 0; i<size; i++) {
+                System.out.print(tempNode.nodeValue);
+                if(i != size-1) {
+                    System.out.print(" -> ");
+                }
+                tempNode = tempNode.nodeReference;
+            }
+        } else {
+            System.out.println("\n Circular singly linked list does not exist!");
+        }
     }
 }
--- src/com/datastructures/linkedList/circularSinglyLinkedList/ExecuteCircularSinglyLinkedList.java
+++ src/com/datastructures/linkedList/circularSinglyLinkedList/ExecuteCircularSinglyLinkedList.java
@@ -4,12 +4,20 @@ public class ExecuteCircularSinglyLinkedList {
     public static void main(String[] args) {
         CircularSinglyLinkedListFactory circularSinglyLinkedListFactory = new CircularSinglyLinkedListFactory();
         circularSinglyLinkedListFactory.createNode(7);
-        System.out.println(circularSinglyLinkedListFactory.head.nodeValue);
+        System.out.println("head is " + circularSinglyLinkedListFactory.head.nodeValue);
         circularSinglyLinkedListFactory.insertNode(5,0);
-        System.out.println(circularSinglyLinkedListFactory.head.nodeValue);
-        circularSinglyLinkedListFactory.insertNode(7,7);
-        System.out.println(circularSinglyLinkedListFactory.tail.nodeValue);
+        System.out.println("head is " + circularSinglyLinkedListFactory.head.nodeValue);
+        circularSinglyLinkedListFactory.insertNode(8,7);
+        System.out.println("tail is " + circularSinglyLinkedListFactory.tail.nodeValue);
         circularSinglyLinkedListFactory.insertNode(6,1);
-        System.out.println(circularSinglyLinkedListFactory.head.nodeValue);
+        System.out.println("head is " + circularSinglyLinkedListFactory.head.nodeValue);
+        System.out.println("\n\n******Testing*******");
+        System.out.println("<0> " + circularSinglyLinkedListFactory.head.nodeValue);
+        System.out.println("<1> " + circularSinglyLinkedListFactory.head.nodeReference.nodeValue);
+        System.out.println("<2> " + circularSinglyLinkedListFactory.head.nodeReference.nodeReference.nodeValue);
+        System.out.println("<3> " + circularSinglyLinkedListFactory.head.nodeReference.nodeReference.nodeReference.nodeValue);
+        System.out.println("******Testing*******");
+        System.out.println("\n\nTraversing circular singly linked list --/--");
+        circularSinglyLinkedListFactory.traverseCircularSinglyLinkedList();
     }
 }
