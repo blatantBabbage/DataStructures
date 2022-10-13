ad5bd397bc89efef77842079838244fd90e133fa
blatantBabbage <ransinghbel@gmail.com>
2022-10-14 00:51:15 +0530

    Add insertion method for adding node to doubly linked list

--- src/com/datastructures/linkedList/doublyLinkedList/DoublyLinkedListFactory.java
+++ src/com/datastructures/linkedList/doublyLinkedList/DoublyLinkedListFactory.java
@@ -5,6 +5,7 @@ public class DoublyLinkedListFactory {
     DoublyNode tail;
     int size;
 
+    // create a doubly node
     public void createNode(int valueOfNode) {
         DoublyNode doublyNode = new DoublyNode();
         doublyNode.nodeValue = valueOfNode;
@@ -13,5 +14,40 @@ public class DoublyLinkedListFactory {
         head = doublyNode;
         tail = doublyNode;
         size = 1;
+    }
+
+    /* Insert node in doubly linked list at a required location */
+    public void insertNode(int valueOfNode, int location) {
+        DoublyNode node = new DoublyNode();
+        node.nodeValue = valueOfNode;
+        if(head == null) {
+            createNode(valueOfNode);
+            return;
+        } else if (location == 0) {
+            // insert node at the beginning of list
+            node.prevNode = null;
+            node.nextNode = head;
+            head.prevNode = node;
+            head = node;
+        } else if (location >= size-1) {
+            // insert node at the end of list
+            node.nextNode = null;
+            node.prevNode = tail;
+            tail.nextNode = node;
+            tail = node;
+        } else {
+            // insert node somewhere in between of list
+            DoublyNode tempNode = head;
+            int index = 0;
+            while (index < location-1) {
+                tempNode = tempNode.nextNode;
+                index++;
+            }
+            node.nextNode = tempNode.nextNode;
+            node.prevNode = tempNode;
+            tempNode.nextNode = node;
+            node.nextNode.prevNode = node;
+        }
+        size++;
     }
 }
--- src/com/datastructures/linkedList/doublyLinkedList/ExecuteDoublyLinkedList.java
+++ src/com/datastructures/linkedList/doublyLinkedList/ExecuteDoublyLinkedList.java
@@ -5,5 +5,11 @@ public class ExecuteDoublyLinkedList {
         DoublyLinkedListFactory doublyLinkedListFactory = new DoublyLinkedListFactory();
         doublyLinkedListFactory.createNode(4);
         System.out.println("Value of Head is : " + doublyLinkedListFactory.head.nodeValue);
+        doublyLinkedListFactory.insertNode(5,0);
+        System.out.println("Value of Head is : " + doublyLinkedListFactory.head.nodeValue);
+        doublyLinkedListFactory.insertNode(7,1);
+        System.out.println("Value of tail is : " + doublyLinkedListFactory.tail.nodeValue);
+        doublyLinkedListFactory.insertNode(6,1);
+        System.out.println("Value of tail is : " + doublyLinkedListFactory.tail.nodeValue);
     }
 }
