9bd1acb507bc105795a92172697f427c0a0dae9e
blatantBabbage <ransinghbel@gmail.com>
2024-02-12 21:53:41 +0530

    Add Stack using array

--- src/com/datastructures/stack/Main.java
+++ src/com/datastructures/stack/Main.java
@@ -1,2 +1,21 @@
-package com.datastructures.stack;public class Main {
+package com.datastructures.stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(4);
+        stack.isEmpty();
+        stack.isFull();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        int pop1 = stack.pop();
+        System.out.println("Value popped  is "+pop1);
+        int pop2 = stack.pop();
+        System.out.println("Value popped  is "+pop2);
+        int seek = stack.peek();
+        System.out.println("Value peeked  is "+seek);
+        stack.deleteStack();
+    }
 }
--- src/com/datastructures/stack/Stack.java
+++ src/com/datastructures/stack/Stack.java
@@ -1,2 +1,64 @@
-package com.datastructures.stack;public class Stack {
+package com.datastructures.stack;
+
+public class Stack {
+    int[] arr;
+    int topOfStack;
+
+    public Stack(int size) {
+        this.arr = new int[size];
+        topOfStack = -1;
+        System.out.println("Initialized Array Stack of size " + size);
+    }
+
+    public boolean isEmpty() {
+        if( topOfStack == -1) {
+            System.out.println("Stack is empty");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull() {
+        if( topOfStack == arr.length - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public void push(int value) {
+        if(isFull()) {
+            System.out.println("Stack is full!!");
+        }
+        else {
+            arr[topOfStack+1] = value;
+            topOfStack++;
+            System.out.println("Value is successfully inserted");
+        }
+    }
+
+    public int pop() {
+        if(isEmpty()) {
+            return -1;
+        }
+        else {
+            int valueToPop = arr[topOfStack];
+            topOfStack--;
+            return valueToPop;
+        }
+    }
+
+    public int peek() {
+        if(isEmpty()) {
+            return -1;
+        }
+        else {
+            int topStackValue = arr[topOfStack];
+            return topStackValue;
+        }
+    }
+
+    public void deleteStack(){
+        arr = null;
+        System.out.println("Stack is deleted");
+    }
 }
